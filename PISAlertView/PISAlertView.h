0d1c9f5ef1ebcb8f1d31aaa81f993e4700fa2d20
PisKey <piskey@guantangbaodedazhenglongdeMacBook-Pro.local>
2016-12-08 16:33:03 +0800

    no message

--- PISAlertView/PISAlertView.h
+++ PISAlertView/PISAlertView.h
@@ -5,8 +5,9 @@
 //  Created by 灌汤包的大蒸笼 on 2016/12/8.
 //  Copyright © 2016年 灌汤包的大蒸笼. All rights reserved.
 //
-//  因为iOS8 以后UIAlertView已经不推荐使用 所以自己简单封装了一下UIAlertView 和
-//  UIAlertController 根据系统自动选择 封装的比较简单 为了方便使用而已
+//  因为iOS8 以后UIAlertView已经不推荐使用 但是还需要兼容iOS7
+//  所以自己简单封装了一下UIAlertView 和 UIAlertController
+//  根据系统自动选择 封装的比较简单 为了方便使用而已
 //
 
 #import <Foundation/Foundation.h>
--- PISAlertView/ViewController.m
+++ PISAlertView/ViewController.m
@@ -20,13 +20,20 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     
-    
     UIButton *button1 = [UIButton buttonWithType:UIButtonTypeCustom];
-    button1.frame = CGRectMake(0, 0, 100, 40);
+    button1.frame = CGRectMake(0, 0, 150, 40);
     button1.backgroundColor = [UIColor brownColor];
     [self.view addSubview:button1];
     button1.center = self.view.center;
+    [button1 setTitle:@"alert" forState:UIControlStateNormal];
     [button1 addTarget:self action:@selector(buttonClick) forControlEvents:UIControlEventTouchUpInside];
+    
+    UIButton *button2 = [UIButton buttonWithType:UIButtonTypeCustom];
+    button2.frame = CGRectMake(button1.frame.origin.x, button1.frame.origin.y + 100, 150, 40);
+    button2.backgroundColor = [UIColor grayColor];
+    [button2 setTitle:@"sheet" forState:UIControlStateNormal];
+    [self.view addSubview:button2];
+    [button2 addTarget:self action:@selector(buttonClick2) forControlEvents:UIControlEventTouchUpInside];
 }
 
 - (void)buttonClick
@@ -39,6 +46,18 @@
         NSLog(@"cancel");
     }];
     [alert showWithView:self TypeStyle:PISAlertViewStyleNormal];
+}
+
+- (void)buttonClick2
+{
+    PISAlertView *alert = [[PISAlertView alloc] initWithTitle:@"这是个提示" message:@"提示了很多信息"];
+    [alert addBtnTitle:@"确定" action:^{
+        NSLog(@"确定了");
+    }];
+    [alert addBtnTitle:@"取消" action:^{
+        NSLog(@"cancel");
+    }];
+    [alert showWithView:self TypeStyle:PISAlertViewStyleSheet];
 }
 
 - (void)didReceiveMemoryWarning {
